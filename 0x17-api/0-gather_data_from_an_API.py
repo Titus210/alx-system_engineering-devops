19af92ddae9c947d26011f24b8ab06fa82b1d61a
Titus Kiplagat <87174663+Titus210@users.noreply.github.com>
2023-01-16 23:14:36 +0300

    New

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -7,12 +7,23 @@ name and the tasks the employee completed.
 """
 import requests
 import sys
+
 if __name__ == "__main__":
-	eid = sys.argv[1]
-	 name = requests.get("http://jsonplaceholder.typicode.com/users/{}".format(eid)).json().get("name")
-	    total_tasks = 0
-
+    eid = sys.argv[1]
+    name = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                        .format(eid)).json().get("name")
+    total_tasks = 0
     done_tasks = []
-
     r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
 
+    for task in r:
+        if (task.get("userId") == int(eid)):
+            total_tasks += 1
+            if (task.get("completed")):
+                done_tasks.append(task.get("title"))
+
+    print("Employee {} is done with tasks({:d}/{:d}):"
+          .format(name, len(done_tasks), total_tasks))
+
+    for item in done_tasks:
+        print("\t {}".format(item))
